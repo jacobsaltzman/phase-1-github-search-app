37853336a0e6a148f81778a52294114d352bef25
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-05-27 23:22:32 -0700

    commit tree 46a2d48601b8abf4ca2354c33245be5ebcc01a1b

---
+++ js/index.js
@@ -0,0 +1,49 @@
+const form = document.getElementById("github-form")
+console.log(form)
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    e.target[0].value
+    fetch(`https://api.github.com/search/users?q=${e.target[0].value}`)
+        .then(response => response.json())
+        .then(json => {
+            const userList = document.querySelector("#user-list");
+            const reposList = document.getElementById("repos-list");
+            reposList.innerHTML = ""
+            userList.innerHTML = ""
+            json.items.map(item => {
+                const li = document.createElement("li");
+                const h2 = document.createElement("h2");
+                h2.textContent = item.login;
+
+                h2.addEventListener("click", e => showUserRepos(item.login, e))
+                const img = document.createElement("img")
+                img.src = item.avatar_url
+                
+
+
+                li.append(h2, img)
+                userList.append(li)
+
+            })
+        })
+
+        form.reset()
+})
+
+
+function showUserRepos(username, e) {
+    const reposList = document.getElementById("repos-list");
+    reposList.innerHTML = ""
+    e.preventDefault();
+
+    fetch(`https://api.github.com/users/${username}/repos`)
+        .then(response => response.json())
+        .then(response => response.map(repo => {
+            const li = document.createElement("li");
+            const h1 = document.createElement("h1");
+            h1.textContent = repo.name;
+
+            li.append(h1)
+            reposList.append(li)
+        }))
+      }
